7c1b9d22421354832406d1e18586ac68d12895d5
Cat Tran <54409102+ctran20@users.noreply.github.com>
2021-08-16 22:33:51 -0400

    Creating card components

--- src/App.js
+++ src/App.js
@@ -2,7 +2,22 @@ import 'tachyons';
 import './App.css';
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <div className="bg-yellow dib br3 pa3 ma2 grow">
+        <h2>Card</h2>
+      </div>
+      <div className="bg-yellow dib br3 pa3 ma2 grow">
+        <h2>Card</h2>
+      </div>
+      <div className="bg-yellow dib br3 pa3 ma2 grow">
+        <h2>Card</h2>
+      </div>
+      <div className="bg-yellow dib br3 pa3 ma2 grow">
+        <h2>Card</h2>
+      </div>
+    </div>
+  );
 }
 
 export default App;
