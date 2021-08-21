b2c6c9b8403f7e6e9e20b8f660beac8beabfcd77
Cat Tran <54409102+ctran20@users.noreply.github.com>
2021-08-21 12:46:22 -0400

    Adding redux

---
+++ src/actions/card.js
@@ -0,0 +1,17 @@
+export const HIT = 'HIT';
+
+// export const Hit = () => {
+//   const newCards = playerCards.concat({
+//     rank: Ranks[Math.floor(Math.random() * Ranks.length)],
+//     suit: Suits[Math.floor(Math.random() * Suits.length)],
+//   });
+//   setPlayerCards(newCards);
+// };
+
+export const hit = () => ({
+  type: HIT,
+  payload: {
+    rank: Ranks[Math.floor(Math.random() * Ranks.length)],
+    suit: Suits[Math.floor(Math.random() * Suits.length)],
+  },
+});
